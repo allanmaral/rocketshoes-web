f225c040ecff8c12bdf7917c0da6dd02c484b06d
Allan Amaral <allanmaral@hotmail.com>
2019-11-05 23:11:06 -0300

    feat: Added Cart page template
    Created the cart page template with a table of itens

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
+
+import tenisImage from '../../assets/images/sample_tenis.jpg';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th> </th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={tenisImage} alt="Tênis" />
+            </td>
+            <td>
+              <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+              <span>R$ 409,99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 819,98</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">FINALIZAR PEDIDO</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,99</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
