80156cd31314c1d8d516d67fdabe779b5bfb3667
Allan Amaral <allanmaral@hotmail.com>
2019-11-05 22:15:00 -0300

    feat: Created the Home page template
    Defined the item list template in the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
+import tenisImage from '../../assets/images/sample_tenis.jpg';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img src={tenisImage} alt="Tênis" />
+        <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+        <span>R$ 409,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={tenisImage} alt="Tênis" />
+        <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+        <span>R$ 409,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={tenisImage} alt="Tênis" />
+        <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+        <span>R$ 409,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={tenisImage} alt="Tênis" />
+        <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+        <span>R$ 409,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={tenisImage} alt="Tênis" />
+        <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+        <span>R$ 409,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={tenisImage} alt="Tênis" />
+        <strong>Tênis Nike Shox Nz Eu Masculino - Preto</strong>
+        <span>R$ 409,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
